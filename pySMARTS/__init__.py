a4c20780c25ebc27458825bffa4ae118f36b8616
Silvana Ayala <silvana.ayala@nrel.gov>
2021-05-03 10:23:26 -0600

    add versioneer, update setup

---
+++ pySMARTS/__init__.py
@@ -0,0 +1,4 @@
+from pySMARTS.main import SMARTSTimeLocation, SMARTSAirMass, SMARTSSpectraZenAzm, SMARTSTMY3, SMARTSSRRL
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
